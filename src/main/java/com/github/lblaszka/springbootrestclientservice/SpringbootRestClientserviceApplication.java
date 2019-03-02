699d950c5987161071816d70725b96aad49686f1
lblaszka <mlblaszka@gmail.com>
2019-03-02 12:38:03 +0100

    Add springboot files

---
+++ src/main/java/com/github/lblaszka/springbootrestclientservice/SpringbootRestClientserviceApplication.java
@@ -0,0 +1,13 @@
+package com.github.lblaszka.springbootrestclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestClientserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestClientserviceApplication.class, args);
+	}
+
+}
